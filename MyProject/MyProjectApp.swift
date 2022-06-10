57449f08c39182ca8e0ce2ff95e3d7088108903f
alexliou <alexliou0@gmail.com>
2022-06-10 16:22:48 -0400

    Initial Commit

---
+++ MyProject/MyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProjectApp.swift
+//  MyProject
+//
+//  Created by Alex Liou on 6/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
