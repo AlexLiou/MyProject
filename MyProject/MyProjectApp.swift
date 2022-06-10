77d1c60d786bbc791d0c788aef50e23302d9f0be
alexliou <alexliou0@gmail.com>
2022-06-10 17:40:27 -0400

    Initial Commit.

--- MyProject/MyProjectApp.swift
+++ MyProject/MyProjectApp.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 @main
 struct MyProjectApp: App {
+    // @StateObject because our app will create and own the data controller, ensuring it stays alive for the duration of the our app's runtime.
+    @StateObject var dataController: DataController
     var body: some Scene {
         WindowGroup {
             ContentView()
+            // Connects CoreData to SwifutUI by letting SwiftUI know where to look for the data
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
         }
+    }
+    
+    init() {
+        let dataController = DataController()
+        _dataController = StateObject(wrappedValue: dataController)
     }
 }
