039e2798009164fe7e6105d1386a6b305efbaf3e
Alex Liou <alexliou0@gmail.com>
2022-06-17 20:52:23 -0400

    Add files via upload

---
+++ MyProject/Activites/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  MyProject
+//
+//  Created by Alex Liou on 6/10/22.
+//
+
+import SwiftUI
+
+/// The main content View, uses a TabView to switch between the HomeView, ProjectsView (closed or open),
+/// and AwardsView.
+struct ContentView: View {
+    @SceneStorage("selectedView") var selectedView: String?
+    
+    var body: some View {
+        TabView(selection: $selectedView) {
+            HomeView()
+                .tag(ProjectsViewModel.tag)
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            ProjectsView(showClosedProjects: false)
+                .tag(ProjectsViewModel.openTag)
+                .tabItem {
+                    Image(systemName: "list.bullet")
+                    Text("Open")
+                }
+            ProjectsView(showClosedProjects: true)
+                .tag(ProjectsViewModel.closedTag)
+                .tabItem {
+                    Image(systemName: "checkmark")
+                    Text("Closed")
+                }
+            AwardsView()
+                .tag(AwardsView.tag)
+                .tabItem {
+                    Image(systemName: "rosette")
+                    Text("Awards")
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var dataController = DataController.preview
+    
+    static var previews: some View {
+        ContentView()
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
+    }
+}
